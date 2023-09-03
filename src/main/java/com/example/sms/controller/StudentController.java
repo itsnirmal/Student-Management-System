9b961fde7ebcd9eb93ccde02dd1ae0c5a4c6d8d7
Nirmal Gautam <66936373+itsnirmal@users.noreply.github.com>
2023-09-02 22:57:31 -0500

    Added student update feature

--- src/main/java/com/example/sms/controller/StudentController.java
+++ src/main/java/com/example/sms/controller/StudentController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.sms.entity.Student;
@@ -41,5 +42,29 @@ public class StudentController {
 		return "redirect:/students";
 	}
 	
+	@GetMapping("/students/edit/{id}")
+	public String editStudentForm(@PathVariable Long id, Model model) {
+		
+		model.addAttribute("student", studentService.getStudentById(id));
+		return "edit_student";
+	}
+	
+	@PostMapping("/students/{id}")
+	public String updateStudent(@PathVariable Long id, 
+			@ModelAttribute("student") Student student,
+					Model model) {
+		
+		//get student database by id
+		Student existingStudent = studentService.getStudentById(id);
+		existingStudent.setId(id);
+		existingStudent.setFirstName(student.getFirstName());
+		existingStudent.setLastName(student.getLastName());
+		existingStudent.setEmail(student.getEmail());
+		
+		//save updates
+		studentService.saveStudent(existingStudent);
+		
+		return "redirect:/students";
+	}
 	
 }
